f2a7a2276ac7009edce1843dc81363e4e08c1ff9
Tero Marttila <terom@fixme.fi>
2014-01-15 19:31:52 +0200

    common/log: drop unused err

--- src/common/log.c
+++ src/common/log.c
@@ -6,12 +6,11 @@
 void _log_msg (const char *prefix, const char *fmt, ...)
 {
 	va_list args;
-	int err;
 
 	fprintf(stderr, "%s: ", prefix);
 
 	va_start(args, fmt);
-	err = vfprintf(stderr, fmt, args);
+	vfprintf(stderr, fmt, args);
 	va_end(args);
 
 	fprintf(stderr, "\n");
