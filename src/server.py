e7b325b3867232a1949bbe0deabfe1a765fedc5d
Tero Marttila <terom@fixme.fi>
2014-01-16 00:05:46 +0200

    server.py: send headers + data

--- src/server.py
+++ src/server.py
@@ -5,6 +5,9 @@ import BaseHTTPServer
 class Handler (BaseHTTPServer.BaseHTTPRequestHandler) :
 	def do_GET (self) :
 		self.send_response(200, "OK")
+		self.send_header("X-Test", "Testing")
+		self.end_headers()
+		self.wfile.write("Data\r\n")
 
 def main (listen='localhost', port=8080) :
 	server = BaseHTTPServer.HTTPServer((listen, port), Handler)
