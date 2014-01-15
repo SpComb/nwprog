eace16252d5fae7eda3f4fe81d6d9099f04ff6c7
Tero Marttila <terom@fixme.fi>
2014-01-15 20:56:05 +0200

    src/common/sock: socket-related utilities

---
+++ src/common/sock.c
@@ -0,0 +1,74 @@
+#include "common/sock.h"
+
+#include "common/log.h"
+
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+int sockaddr_buf (char *buf, size_t buflen, const struct sockaddr *sa, socklen_t salen)
+{
+	char host[NI_MAXHOST];
+	char serv[NI_MAXSERV];
+	int err;
+	
+	if ((err = getnameinfo(sa, salen, host, sizeof(host), serv, sizeof(serv), NI_NUMERICHOST | NI_NUMERICSERV))) {
+		log_warning("getnameinfo: %s", gai_strerror(err));
+		return -1;
+	}
+
+	snprintf(buf, buflen, "%s:%s", host, serv);
+
+	return 0;
+}
+
+const char *sockaddr_str (const struct sockaddr *sa, socklen_t salen)
+{
+	static char buf[SOCKADDR_MAX];
+
+	if (sockaddr_buf(buf, sizeof(buf), sa, salen)) {
+		return NULL;
+	}
+
+	return buf;
+}
+
+const char * sockname_str (int sock)
+{
+	static char buf[SOCKADDR_MAX];
+
+	struct sockaddr_storage addr;
+	socklen_t addrlen;
+
+	if (getsockname(sock, (struct sockaddr *) &addr, &addrlen)) {
+		log_pwarning("getsockname(%d)", sock);
+		return NULL;
+	}
+
+	if (sockaddr_buf(buf, sizeof(buf), (struct sockaddr *) &addr, addrlen)) {
+		return NULL;
+	}
+
+	return buf;
+}
+
+const char * sockpeer_str (int sock)
+{
+	static char buf[SOCKADDR_MAX];
+
+	struct sockaddr_storage addr;
+	socklen_t addrlen;
+
+	if (getpeername(sock, (struct sockaddr *) &addr, &addrlen)) {
+		log_pwarning("getpeername(%d)", sock);
+		return NULL;
+	}
+
+	if (sockaddr_buf(buf, sizeof(buf), (struct sockaddr *) &addr, addrlen)) {
+		return NULL;
+	}
+
+	return buf;
+
+}
---
+++ src/common/sock.h
@@ -0,0 +1,27 @@
+#ifndef SOCK_H
+#define SOCK_H
+
+#include <sys/socket.h>
+
+#define SOCKADDR_MAX 1024
+
+/*
+ * Format host:srv for given sockaddr into given buf, should should be at least SOCKADDR_MAX len.
+ */
+int sockaddr_buf (char *buf, size_t buflen, const struct sockaddr *sa, socklen_t salen);
+
+/*
+ * Return a (static) pointer to the host/serv for the given sockaddr.
+ */
+const char * sockaddr_str (const struct sockaddr *sa, socklen_t salen);
+
+/*
+ * Return a (static) pointer to the local host:serv for the given socket.
+ */
+const char * sockname_str (int sock);
+/*
+ * Return a (static) pointer to the remote host:serv for the given socket.
+ */
+const char * sockpeer_str (int sock);
+
+#endif
