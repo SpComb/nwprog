247da16a5fdea00f2ca9a5d93857f16d99c3d5b6
Tero Marttila <terom@fixme.fi>
2014-01-16 19:48:04 +0200

    server: barebones request/response

--- src/server/server.c
+++ src/server/server.c
@@ -6,10 +6,31 @@
 #include "common/tcp.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 struct server {
 	int sock;
+};
+
+struct server_request {
+	const char *method;
+	const char *path;
+	const char *version;
+
+	/* Headers */
+	size_t content_length;
+};
+
+struct server_response {
+	unsigned status;
+	const char *reason;
+
+	/* Headers */
+	size_t content_length;
+
+	/* Write response content to FILE */
+	FILE *content_file;
 };
 
 int server_create (struct server **serverp, const char *host, const char *port)
@@ -35,28 +56,78 @@ error:
 	return -1;
 }
 
-int server_request (struct server *server, struct http *http)
+/*
+ * Read in request
+ */
+int server_request (struct server *server, struct http *http, struct server_request *request)
 {
 	int err;
 
-	const char *method, *path, *version;
-
-	if (http_read_request(http, &method, &path, &version)) {
+	if (http_read_request(http, &request->method, &request->path, &request->version)) {
 		log_error("http_read_request");
 		return 1;
 	}
 
-	log_info("%s %s %s", method, path, version);
+	log_info("%s %s %s", request->method, request->path, request->version);
 
 	// headers
-	const char *name, *value;
+	const char *header, *value;
 
-	while (!(err = http_read_header(http, &name, &value))) {
-		log_info("\t%20s : %s", name, value);
+	while (!(err = http_read_header(http, &header, &value))) {
+		log_info("\t%20s : %s", header, value);
+
+		if (strcasecmp(header, "Content-Length") == 0) {
+			if (sscanf(value, "%zu", &request->content_length) != 1) {
+				log_warning("invalid content_length: %s", value);
+				return 1;
+			}
+
+			log_debug("content_length=%zu", request->content_length);
+		}
 	}
 
 	if (err < 0) {
 		log_error("http_read_header");
+		return 1;
+	}
+	
+	// determine if the request includes a body
+	// TODO: Transfer-Encoding
+	if (!request->content_length) {
+		log_debug("not expecting a request body");
+
+	} else {
+	   	if (((err = http_read_file(http, NULL, request->content_length)))){
+			log_error("http_read_file");
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * Send response.
+ */
+int server_response (struct server *server, struct http *http, struct server_response *response)
+{
+	if (http_write_response(http, response->status, response->reason)) {
+		log_error("failed to write response line");
+		return 1;
+	}
+
+	if (response->content_length && http_write_header(http, "Content-Length", "%zu", response->content_length)) {
+		log_error("failed to write response header line");
+		return 1;
+	}
+
+	if (http_write_headers(http)) {
+		log_error("failed to write end-of-headers");
+		return 1;
+	}
+
+	if (response->content_file && http_write_file(http, response->content_file, response->content_length)) {
+		log_error("failed to write response body");
 		return 1;
 	}
 
@@ -88,7 +159,18 @@ int server_run (struct server *server)
 		goto error;
 	}
 
-	if ((err = server_request(server, http))) {
+	struct server_request request = { };
+
+	if ((err = server_request(server, http, &request))) {
+		goto error;
+	}
+	
+	struct server_response response = {
+		.status		= 200,
+		.reason		= "OK",
+	};
+
+	if ((err = server_response(server, http, &response))) {
 		goto error;
 	}
 
