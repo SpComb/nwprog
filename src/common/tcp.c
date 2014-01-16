de074ebdecb73646d12b71559481bd228ab0d28f
Tero Marttila <terom@fixme.fi>
2014-01-16 19:07:56 +0200

    tcp: tcp_listen

--- src/common/tcp.c
+++ src/common/tcp.c
@@ -50,3 +50,57 @@ int tcp_connect (const char *host, const char *port)
 	// either -1 or valid
 	return sock;
 }
+
+int tcp_listen (const char *host, const char *port, int backlog)
+{
+	int err;
+	struct addrinfo hints = {
+		.ai_flags		= AI_PASSIVE,
+		.ai_family		= AF_UNSPEC,
+		.ai_socktype	= SOCK_STREAM,
+		.ai_protocol	= 0,
+	};
+	struct addrinfo *addrs, *addr;
+	int sock = -1;
+
+	if ((err = getaddrinfo(host, port, &hints, &addrs))) {
+		log_perror("getaddrinfo %s:%s: %s", host, port, gai_strerror(err));
+		return -1;
+	}
+
+	for (addr = addrs; addr; addr = addr->ai_next) {
+		if ((sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol)) < 0) {
+			log_pwarning("socket(%d, %d, %d)", addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+			continue;
+		}
+
+		log_info("%s...", sockaddr_str(addr->ai_addr, addr->ai_addrlen));
+		
+		// bind to listen address/port
+		if ((err = bind(sock, addr->ai_addr, addr->ai_addrlen)) < 0) {
+			log_pwarning("bind");
+			close(sock);
+			sock = -1;
+			continue;
+		}
+
+		log_info("%s", sockname_str(sock));
+
+		break;
+	}
+
+	freeaddrinfo(addrs);
+
+	if (sock < 0)
+		return sock;
+	
+	// mark as listening
+	if ((err = listen(sock, backlog))) {
+		log_perror("listen");
+		close(sock);
+		sock = -1;
+	}
+
+	// either -1 or valid
+	return sock;
+}
--- src/common/tcp.h
+++ src/common/tcp.h
@@ -1,11 +1,21 @@
 #ifndef TCP_H
 #define TCP_H
 
+/* This is a number with far too low a level of entropy to be used as a random number */
+#define TCP_LISTEN_BACKLOG 10
+
 /*
  * Open a TCP socket and connect to given host/port.
  *
  * Returns connected SOCK_STREAM socket fd, or <0 on error, with errno set.
  */
 int tcp_connect (const char *host, const char *port);
+
+/*
+ * Open a TCP server socket, listening on the given host/port.
+ *
+ * host may be given as NULL to listen on all addresses.
+ */
+int tcp_listen (const char *host, const char *port, int backlog);
 
 #endif
