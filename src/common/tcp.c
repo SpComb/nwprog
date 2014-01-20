691dbe853fe02347bf7d4dbd1f9732acaff55598
Tero Marttila <terom@fixme.fi>
2014-01-21 01:29:53 +0200

    tcp: use event_yield to implement tcp_stream_read/write

--- src/common/tcp.c
+++ src/common/tcp.c
@@ -19,6 +19,51 @@ void tcp_event (struct event *event, int flags, void *ctx)
 	log_info("flags=%#02x", flags);
 }
 
+int tcp_stream_read (char *buf, size_t *sizep, void *ctx)
+{
+    struct tcp *tcp = ctx;
+    int err;
+
+    while ((err = sock_read(tcp->sock, buf, sizep)) > 0) {
+        if (event_yield(tcp->event, EVENT_READ)) {
+            log_error("event_yield");
+            return -1;
+        }
+    }
+
+    if (err) {
+        log_error("sock_read");
+        return -1;
+    }
+
+    return 0;
+}
+
+int tcp_stream_write (const char *buf, size_t *sizep, void *ctx)
+{
+    struct tcp *tcp = ctx;
+    int err;
+
+    while ((err = sock_write(tcp->sock, buf, sizep)) > 0) {
+        if (event_yield(tcp->event, EVENT_WRITE)) {
+            log_error("event_write");
+            return -1;
+        }
+    }
+
+    if (err) {
+        log_error("sock_write");
+        return -1;
+    }
+
+    return 0;
+}
+
+static const struct stream_type tcp_stream_type = {
+    .read   = tcp_stream_read,
+    .write  = tcp_stream_write,
+};
+
 int tcp_create (struct event_main *event_main, struct tcp **tcpp, int sock)
 {
 	struct tcp *tcp;
@@ -30,18 +75,23 @@ int tcp_create (struct event_main *event_main, struct tcp **tcpp, int sock)
 
 	tcp->sock = sock;
 
+    if (sock_nonblocking(sock)) {
+        log_error("sock_nonblocking");
+        goto error;
+    }
+
 	if (event_create(event_main, &tcp->event, sock)) {
-		log_warning("event_create");
+		log_error("event_create");
 		goto error;
 	}
 
-	if (stream_create(&tcp->read, sock, TCP_STREAM_SIZE)) {
-		log_warning("stream_create read");
+	if (stream_create(&tcp_stream_type, &tcp->read, TCP_STREAM_SIZE, tcp)) {
+		log_error("stream_create read");
 		goto error;
 	}
 	
-	if (stream_create(&tcp->write, sock, TCP_STREAM_SIZE)) {
-		log_warning("stream_create write");
+	if (stream_create(&tcp_stream_type, &tcp->write, TCP_STREAM_SIZE, tcp)) {
+		log_error("stream_create write");
 		goto error;
 	}
 
