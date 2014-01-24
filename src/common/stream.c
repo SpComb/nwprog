6fdf1c290413228f0539cdbbd615daf02a990f12
Tero Marttila <terom@fixme.fi>
2014-01-24 23:41:41 +0200

    stream: stream_write_file support; cleanup buf/size using inline functions

--- src/common/stream.c
+++ src/common/stream.c
@@ -8,6 +8,46 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
+ * Read buffer start, for reading new data into the stream.
+ */
+static inline char * stream_readbuf_ptr (struct stream *stream)
+{
+    return stream->buf + stream->length;
+}
+
+/*
+ * Read buffer size, for reading new data into the stream.
+ */
+static inline size_t stream_readbuf_size (struct stream *stream)
+{
+    return stream->size - stream->length;
+}
+
+/*
+ * Write buffer start, for reading old data from the stream.
+ */
+static inline char * stream_writebuf_ptr (struct stream *stream)
+{
+    return stream->buf + stream->offset;
+}
+
+/*
+ * Write buffer size, for reading old data from the stream.
+ */
+static inline size_t stream_writebuf_size (struct stream *stream)
+{
+    return stream->length - stream->offset;
+}
+
+/*
+ * Write buffer end, for reading old data from the stream.
+ */
+static inline char * stream_writebuf_end (struct stream *stream)
+{
+    return stream->buf + stream->length;
+}
+
 int stream_create (const struct stream_type *type, struct stream **streamp, size_t size, void *ctx)
 {
     struct stream *stream;
@@ -46,24 +86,45 @@ int stream_init (const struct stream_type *type, struct stream *stream, size_t s
 }
 
 /*
- * Make space in stream for more data.
- *
- * Returns 1 if the stream buffer is full.
+ * Mark given readbuf bytes as valid.
  */
-int _stream_shift (struct stream *stream)
+inline static void stream_read_mark (struct stream *stream, size_t size)
 {
-    if (stream->offset) {
-        memmove(stream->buf, stream->buf + stream->offset, stream->length - stream->offset);
+    stream->length += size;
+}
 
-        stream->length -= stream->offset;
-        stream->offset = 0;
+/*
+ * Mark given writebuf bytes as consumed.
+ */
+inline static void stream_write_mark (struct stream *stream, size_t size)
+{
+    stream->offset += size;
+}
+
+/*
+ * Clean out marked write buffer, making more room for the read buffer.
+ *
+ * Returns -1 if the stream buffer is full.
+ */
+int _stream_clear (struct stream *stream)
+{
+    if (!(stream->offset || stream->length < stream->size)) {
+        log_warning("stream write buffer is full, no room for read");
+        return -1;
     }
+
+    memmove(stream->buf, stream->buf + stream->offset, stream->length - stream->offset);
+
+    stream->length -= stream->offset;
+    stream->offset = 0;
 
     return 0;
 }
 
 /*
- * Read into stream from fd.
+ * Read into stream from fd. There must be room available for the read buffer.
+ *
+ * This function is guaranteed to make progress.
  *
  * Returns 1 on EOF.
  */
@@ -71,24 +132,24 @@ int _stream_read (struct stream *stream)
 {
     int err;
 
-    // XXX: make room if needed
-    if ((err = _stream_shift(stream)))
+    // make room if needed
+    if ((err = _stream_clear(stream)))
         return err;
     
     // fill up
-    size_t len = stream->size - stream->length;
+    size_t size = stream_readbuf_size(stream);
 
-    if ((err = stream->type->read(stream->buf + stream->length, &len, stream->ctx))) {
+    if ((err = stream->type->read(stream_readbuf_ptr(stream), &size, stream->ctx))) {
         log_pwarning("stream-read");
         return err;
     }
 
-    if (!len) {
+    if (!size) {
         log_debug("eof");
         return 1;
     }
 
-    stream->length += len;
+    stream_read_mark(stream, size);
 
     return 0;
 }
@@ -117,21 +178,24 @@ int _stream_write_direct (struct stream *stream, const char *buf, size_t size)
 
 int _stream_write (struct stream *stream)
 {
-    while (stream->offset < stream->length) {
-        size_t len = stream->length - stream->offset;
+    size_t size = stream_writebuf_size(stream);
 
-        if (stream->type->write(stream->buf + stream->offset, &len, stream->ctx)) {
-            log_pwarning("stream-write");
-            return -1;
-        }
-
-        if (!len) {
-            log_debug("eof");
-            return 1;
-        }
-
-        stream->offset += len;
+    if (!size) {
+        // XXX
+        log_warning("write without anything to write");
+        return 0;
     }
+
+    if (stream->type->write(stream_writebuf_ptr(stream), &size, stream->ctx)) {
+        return -1;
+    }
+
+    if (!size) {
+        log_debug("eof");
+        return 1;
+    }
+
+    stream_write_mark(stream, size);
 
     return 0;
 }
@@ -140,7 +204,7 @@ int stream_read (struct stream *stream, char **bufp, size_t *sizep)
 {
     int err;
     
-    // do readz
+    // until we have the request amount of data, or any data, or EOF
     while (stream->length < stream->offset + *sizep) {
         if ((err = _stream_read(stream)) < 0)
             return err;
@@ -152,14 +216,17 @@ int stream_read (struct stream *stream, char **bufp, size_t *sizep)
     *bufp = stream->buf + stream->offset;
 
     if (*sizep && stream->offset + *sizep < stream->length) {
+        // have full requested size
 
     } else if (stream->length > stream->offset) {
+        // have partial requested size, or however much available
         *sizep = stream->length - stream->offset;
     } else {
 		// EOF
 		return 1;
 	}
-        
+
+    // consumed
     stream->offset += *sizep;
 
     return 0;
@@ -172,7 +239,7 @@ int stream_read_line (struct stream *stream, char **linep)
     
     while (true) {
         // scan for \r\n
-        for (c = stream->buf + stream->offset; c < stream->buf + stream->length; c++) {
+        for (c = stream_writebuf_ptr(stream); c < stream_writebuf_end(stream); c++) {
             if (*c == '\r') {
                 *c = '\0';
             } else if (*c == '\n') {
@@ -197,9 +264,34 @@ out:
     return 0;
 }
 
+/*
+ * Flush any pending writes.
+ */
+int stream_flush (struct stream *stream)
+{
+    int err;
+
+    // empty write buffer
+    while (stream_writebuf_size(stream) > 0) {
+        if ((err = _stream_write(stream)))
+            return err;
+    }
+
+    // drop consumed data from write buffer
+    if ((err = _stream_clear(stream)) < 0)
+        return err;
+
+    return 0;
+}
+
 int stream_write (struct stream *stream, const char *buf, size_t size)
 {
-    // XXX: we assume there is no write buffer
+    int err;
+
+    // our write buffer must be empty, since _stream_write_direct will bypass it
+    if ((err = stream_flush(stream)))
+        return err;
+
     return _stream_write_direct(stream, buf, size);
 }
 
@@ -207,22 +299,22 @@ int stream_vprintf (struct stream *stream, const char *fmt, va_list args)
 {
     int ret, err;
 
-    if ((err = _stream_shift(stream)))
-        return err;
-
-    if ((ret = vsnprintf(stream->buf + stream->length, stream->size - stream->length, fmt, args)) < 0) {
+    if ((ret = vsnprintf(stream_readbuf_ptr(stream), stream_readbuf_size(stream), fmt, args)) < 0) {
         log_perror("snprintf");
         return -1;
     }
-
-    if (stream->length + ret > stream->size) {
+    
+    if (ret >= stream_readbuf_size(stream))
         // full
         return 1;
-    }
+    
+    stream_read_mark(stream, ret);
+    
+    // TODO: write buffering
+    if ((err = stream_flush(stream)))
+        return err;
 
-    stream->length += ret;
-
-    return _stream_write(stream);
+    return 0;
 }
 
 int stream_printf (struct stream *stream, const char *fmt, ...)
@@ -235,6 +327,64 @@ int stream_printf (struct stream *stream, const char *fmt, ...)
     va_end(args);
 
     return ret;
+}
+
+/*
+ * Fallback for sendfile.
+ */
+int _stream_write_file (struct stream *stream, int fd, size_t *sizep)
+{
+    int err;
+    ssize_t ret;
+
+    if ((err = _stream_clear(stream)))
+        return err;
+
+    // read into readbuf
+    size_t size = stream_readbuf_size(stream);
+
+    if (*sizep && *sizep < size)
+        // limit
+        size = *sizep;
+
+    if ((ret = read(fd, stream_readbuf_ptr(stream), size)) < 0) {
+        log_perror("read");
+        return -1;
+    }
+
+    if (!ret) {
+        log_debug("read: eof");
+        return 1;
+    }
+
+    stream_read_mark(stream, ret);
+
+    // update
+    *sizep = ret;
+
+    // write out
+    if ((err = _stream_write(stream)))
+        return err;
+
+    return 0;
+}
+
+int stream_write_file (struct stream *stream, int fd, size_t *sizep)
+{
+    int err;
+
+    if (!stream->type->sendfile)
+        // fallback
+        return _stream_write_file(stream, fd, sizep);
+
+    // our write buffer must be empty, since sendfile will bypass it
+    if ((err = stream_flush(stream)))
+        return err;
+
+    if ((err = stream->type->sendfile(fd, sizep, stream->ctx)))
+        return err;
+
+    return 0;
 }
 
 void stream_destroy (struct stream *stream)
--- src/common/stream.h
+++ src/common/stream.h
@@ -5,18 +5,32 @@
 #include <stdarg.h>
 
 /*
- * Buffered SOCK_STREAMs
+ * Blocking SOCK_STREAM interface.
+ *
+ * These functions should always make some progress, updating *sizep
+ *
+ * Return <0 on error, 1 on EOF, 0 on success.
  */
 struct stream_type {
     int (*read)(char *buf, size_t *sizep, void *ctx);
     int (*write)(const char *buf, size_t *sizep, void *ctx);
+    int (*sendfile)(int fd, size_t *sizep, void *ctx);
 };
 
 struct stream {
     const struct stream_type *type;
 
     char *buf;
-    size_t size, length, offset;
+
+    // note that offset <= length <= size at all times
+    /* The amount of leading data in the buffer that has already been consumed */
+    size_t offset;
+
+    /* The amount of valid data existing in the buffer */
+    size_t length;
+
+    /* The total length of the buffer */
+    size_t size;
 
     void *ctx;
 };
@@ -45,6 +59,16 @@ int stream_read_line (struct stream *stream, char **linep);
 int stream_write (struct stream *stream, const char *buf, size_t size);
 int stream_vprintf (struct stream *stream, const char *fmt, va_list args);
 int stream_printf (struct stream *stream, const char *fmt, ...);
+
+/*
+ * Copy to stream from a file, bypassing the buffer if the stream_type implements it.
+ *
+ * *sizep is the number of bytes to be sent from fd, or zero to send until EOF.
+ * *sizep is updated on return to reflect the amount of bytes copied, which may be less then *sizep.
+ *
+ * Returns <0 on error, 0 on success, >0 on EOF.
+ */
+int stream_write_file (struct stream *stream, int fd, size_t *sizep);
 
 /*
  * Release all resources.
