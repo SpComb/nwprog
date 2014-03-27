d8651c4b1e27391806dbd6adfd2f5c77acd586c1
Tero Marttila <terom@paivola.fi>
2014-03-27 23:01:18 +0200

    client: use a per-task client
    --HG--
    branch : tcp-client

--- src/client.c
+++ src/client.c
@@ -13,9 +13,7 @@ struct options {
     const char *iam;
     enum http_version http_version;
     
-    /* Apply */
-	struct client *client;
-
+    struct event_main *event_main;
 #ifdef WITH_SSL
     struct ssl_main *ssl_main;
 #endif
@@ -75,8 +73,9 @@ void client (void *ctx)
     struct client_task *task = ctx;
     const struct options *options = task->options;
 	struct urlbuf urlbuf;
+	struct client *client;
 	FILE *get_file = stdout, *put_file = NULL;
-	int ret = 0;
+	int err = 0;
 
 	if (urlbuf_parse(&urlbuf, task->arg)) {
 		log_fatal("invalid url: %s", task->arg);
@@ -88,84 +87,113 @@ void client (void *ctx)
 		urlbuf.url.path = "";
 	}
 
-    // connect?
-    if (urlbuf.url.host && *urlbuf.url.host) {
-        if (client_open(options->client, &urlbuf.url)) {
-            log_fatal("client_open: %s", task->arg);
-            return 1;
-        }
+    // setup client
+	if (client_create(options->event_main, &client)) {
+		log_fatal("failed to initialize client");
+        return 2;
+	}
+#ifdef WITH_SSL
+    if (client_set_ssl(client, options->ssl_main)) {
+        log_fatal("failed to initialize client ssl");
+        err = 2;
+        goto error;
     }
+#endif
+
+    if (client_set_request_version(client, options->http_version)) {
+        log_fatal("failed to set client request http version");
+        err = 2;
+        goto error;
+    }
+
+	if (options->iam && client_add_header(client, "Iam", options->iam)) {
+		log_fatal("failed to set client Iam header");
+		err = 2;
+		goto error;
+	}
+
+    // connect?
+    //if (urlbuf.url.host && *urlbuf.url.host) {
+        if (client_open(client, &urlbuf.url)) {
+            log_fatal("client_open: %s", task->arg);
+            err = 2;
+            goto error;
+        }
+    //}
 
 	if (options->get) {
         if (!(get_file = fopen(options->get, "w"))) {
             log_error("fopen %s", options->get);
             log_fatal("failed to open --get file for writing");
-            return 2;
+            err = 3;
+            goto error;
         }
         
         // set, with close
-        if (client_set_response_file(options->client, get_file, true)) {
+        if (client_set_response_file(client, get_file, true)) {
             log_fatal("failed to set client response file");
-            return 2;
+            err = 3;
+            goto error;
         }
     } else {
         // default
         get_file = stdout;
 
         // set, without close
-        if (client_set_response_file(options->client, get_file, false)) {
+        if (client_set_response_file(client, get_file, false)) {
             log_fatal("failed to set client response file");
-            return 2;
+            err = 3;
+            goto error;
         }
     }
 
 	if (options->put && !(put_file = fopen(options->put, "r"))) {
 		log_error("fopen %s", options->put);
 		log_fatal("failed to open --put file for reading");
-		return 2;
+        err = 3;
+        goto error;
 	}
 
 	
 	if (put_file) {
-		if ((ret = client_put(options->client, &urlbuf.url, put_file)) < 0) {
+		if ((err = client_put(client, &urlbuf.url, put_file)) < 0) {
 			log_fatal("PUT failed: %s", task->arg);
-            return 3;
+            err = 3;
+            goto error;
 		}
 
 	} else {
-		if ((ret = client_get(options->client, &urlbuf.url)) < 0) {
+		if ((err = client_get(client, &urlbuf.url)) < 0) {
 			log_fatal("GET failed: %s", task->arg);
-            return 3;
+            err = 3;
+            goto error;
 		}
 	}
 
-    if (ret >= 200 && ret < 300) {
-        log_debug("Server returned 2xx response: %d", ret);
+    if (err >= 200 && err < 300) {
+        log_debug("Server errurned 2xx response: %d", err);
 
-        return 0;
+        err = 0;
 
-    } else if (ret >= 300 && ret < 400) {
-        log_info("Server returned 3xx response: %d", ret);
-        
-        return ret;
+    } else if (err >= 300 && err < 400) {
+        log_info("Server errurned 3xx response: %d", err);
 
-    } else if (ret >= 400 && ret < 500) {
-        log_error("Server returned 4xx response: %d", ret);
-        
-        return ret;
+    } else if (err >= 400 && err < 500) {
+        log_error("Server errurned 4xx response: %d", err);
 
-    } else if (ret >= 500 && ret < 600) {
-        log_warning("Server returned 5xx response: %d", ret);
-
-        return ret;
+    } else if (err >= 500 && err < 600) {
+        log_warning("Server errurned 5xx response: %d", err);
 
     } else {
-        log_warning("Server returned unknown response type: %d", ret);
+        log_warning("Server errurned unknown response type: %d", err);
 
-        return -1;
+        err = -1;
     }
 
-    return 0;
+error:
+    client_destroy(client);
+
+    return err;
 }
 
 int main (int argc, char **argv)
@@ -181,8 +209,6 @@ int main (int argc, char **argv)
 
         .http_version   = HTTP_10,
     };
-
-    struct event_main *event_main;
 
 	while ((opt = getopt_long(argc, argv, "hqvdG:P:I:", long_options, NULL)) >= 0) {
 		switch (opt) {
@@ -227,7 +253,7 @@ int main (int argc, char **argv)
 	// apply
 	log_set_level(log_level);
 
-    if ((err = event_main_create(&event_main))) {
+    if ((err = event_main_create(&options.event_main))) {
         log_fatal("event_main_create");
         return 1;
     }
@@ -239,52 +265,23 @@ int main (int argc, char **argv)
     }
 #endif
 
-    // setup client
-	if (client_create(event_main, &options.client)) {
-		log_fatal("failed to initialize client");
-		err = 2;
-		goto error;
-	}
-#ifdef WITH_SSL
-    if (client_set_ssl(options.client, options.ssl_main)) {
-        log_fatal("failed to initialize client ssl");
-        err = 2;
-        goto error;
-    }
-#endif
-
-    if (client_set_request_version(options.client, options.http_version)) {
-        log_fatal("failed to set client request http version");
-        err = 2;
-        goto error;
-    }
-
-	if (options.iam && client_add_header(options.client, "Iam", options.iam)) {
-		log_fatal("failed to set client Iam header");
-		err = 2;
-		goto error;
-	}
-
 	while (optind < argc && !err) {
         struct client_task task = {
             .options    = &options,
             .arg        = argv[optind++],
         };
 
-        if ((err = event_start(event_main, client, &task))) {
+        if ((err = event_start(options.event_main, client, &task))) {
             log_fatal("event_start");
             goto error;
         }
 	}
 
-    if ((err = event_main_run(event_main))) {
+    if ((err = event_main_run(options.event_main))) {
         log_fatal("event_main_run");
         goto error;
     }
 
 error:
-    if (options.client)
-        client_destroy(options.client);
-
 	return err;
 }
